f78d19b0bfe7914cbceb751b189ba353d8148177
SivapriyaKumar <k.sivapriya@hotmail.com>
2019-01-05 22:25:00 +0530

    modified the result and expected message

--- src/EarlySuccessclass.java
+++ src/EarlySuccessclass.java
@@ -10,12 +10,12 @@ import org.testng.annotations.Test;
 
 public class EarlySuccessclass {
 	
-	String path = System.getProperty("user.dir");
+	String chromepath = System.getProperty("user.dir");
 	WebDriver wd ;
 	
 	@BeforeTest
 	public void chromeDriverSetup(){
-		System.setProperty("webdriver.chrome.driver", path+"/Drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", chromepath+"/Drivers/chromedriver.exe");
 		wd = new ChromeDriver();
 	}
 	
@@ -29,9 +29,12 @@ public class EarlySuccessclass {
 	public void searchTest() {
 		wd.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("iphone");
 		wd.findElement(By.xpath("//input[@type='submit' and @class='nav-input']")).click();
-		String actualText = wd.findElement(By.xpath("//h2[@data-attribute='Apple iPhone 6 (Space Grey, 32GB)']/parent::a")).getText().trim();
-		wd.findElement(By.xpath("//h2[@data-attribute='Apple iPhone 6 (Space Grey, 32GB)']/parent::a")).click();
-		String expectedText = "Apple iPhone 6 (Space Grey, 32GB)";
+		//String actualText = wd.findElement(By.xpath("//h2[@data-attribute='Apple iPhone 6 (Gold, 1GB RAM, 32GB Storage)']/parent::a")).getText().trim();
+		WebElement result1 = wd.findElement(By.xpath("//h2[@data-attribute='Apple iPhone 6 (Gold, 1GB RAM, 32GB Storage)']/parent::a"));
+		String actualText = result1.getText().trim();
+		result1.click();
+		//wd.findElement(By.xpath("//h2[@data-attribute='//h2[@data-attribute='Apple iPhone 6 (Gold, 1GB RAM, 32GB Storage)']/parent::a")).click();
+		String expectedText = "Apple iPhone 6 (Gold, 1GB RAM, 32GB Storage)";
 		Assert.assertEquals(actualText, expectedText);
 	}
 	
